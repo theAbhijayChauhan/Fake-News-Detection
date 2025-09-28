a9515e285c56cc43001e310f2ff3256acdf636fc
Abhijay Chauhan <155216391+theAbhijayChauhan@users.noreply.github.com>
2025-09-28 12:47:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
+import os
 # from dotenv import load_dotenv
 
-import os
 
-NEWS_API_KEY = os.environ.get('NEWS_API_KEY', 'd6ffa963ec1b4adc85210666fc37c49') = d
-
-# Other configuration settings
-DEBUG = True
-USERS_FILE = "users.txt"
-HISTORY_FILE = "history.txt"
+NEWS_API_KEY = "d6ffa963ec1b4adc85210666fc37c49" d
